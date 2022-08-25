6617fc0878f81bcae48f77e97195a27c0bf96640
rruiz88 <roman.ruiz816@gmail.com>
2022-08-25 10:34:08 -0500

    npm changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
@@ -16,6 +16,6 @@ import { OrbitCountsComponent } from './orbit-counts/orbit-counts.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
+  
 })
 export class AppModule { }
